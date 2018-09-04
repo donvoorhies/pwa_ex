54fbb5a52f902acd130875336c60234960e4ee94
Don Voorhies <donvoorhies@gmail.com>
2018-09-04 16:52:05 +0200

    Add files via upload

---
+++ pwabuilder-sw.js
@@ -0,0 +1,28 @@
+//This is the "Offline page" service worker
+
+//Install stage sets up the offline page in the cahche and opens a new cache
+self.addEventListener('install', function(event) {
+ "use strict";
+  var offlinePage = new Request('offline.html');
+  event.waitUntil(
+  fetch(offlinePage).then(function(response) {
+    return caches.open('pwabuilder-offline').then(function(cache) {
+      console.log('[PWA Builder] Cached offline page during Install'+ response.url);
+      return cache.put(offlinePage, response);
+    });
+  }));
+});
+
+//If any fetch fails, it will show the offline page.
+//Maybe this should be limited to HTML documents?
+self.addEventListener('fetch', function(event) {
+ "use strict";
+  event.respondWith(
+    fetch(event.request).catch(function(error) {
+        console.error( '[PWA Builder] Network request Failed. Serving offline page ' + error );
+        return caches.open('pwabuilder-offline').then(function(cache) {
+          return cache.match('offline.html');
+      });
+    }));
+});
+
